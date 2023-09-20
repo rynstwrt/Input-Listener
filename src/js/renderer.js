ed405076981c0a8d120831a4c41f8a6275c8eb9c
Ryan Stewart <ryanstewartalex@gmail.com>
2023-09-20 18:02:45 -0500

    Add Functionality

--- src/js/renderer.js
+++ src/js/renderer.js
@@ -4,15 +4,48 @@ const audioInputDeviceSelect = document.querySelector("#audio-input-device-selec
 const audioOutputDeviceSelect = document.querySelector("#audio-output-device-select");
 
 const API_NAME = "listenToAudioInputAPI";
+
 let isListening = false;
+
 let startText = stopText = "";
+
 const inputDeviceMap = new Map(),
     outputDeviceMap = new Map();
+
+let currentInputDeviceID = "default";
+let currentOutputDeviceID = "default";
+
+let audio;
+let stream;
+
+
+async function play()
+{
+    stream = await navigator.mediaDevices.getUserMedia({ audio: { deviceId: { exact: currentInputDeviceID } } });
+
+    audio = new Audio();
+    audio.srcObject = stream;
+    await audio.play();
+}
+
+
+function stop()
+{
+    audio = undefined;
+
+    stream.getTracks().forEach(track =>
+    {
+        if (track.readyState === 'live' && track.kind === 'audio')
+            track.stop();
+    });
+}
 
 
 startStopButton.addEventListener("click", () =>
 {
     isListening = !isListening;
+
+    isListening ? play() : stop();
     startStopButton.textContent = (isListening ? stopText : startText);
 });
 
@@ -31,16 +64,48 @@ async function createAudioDeviceOptions()
         if (!isAudioInput && !isAudioOutput)
             continue;
 
+        // if (device.label.includes("Default - "))
+        // {
+        //     if (isAudioInput)
+        //     {
+        //         currentInputDeviceID = device.deviceId;
+        //     }
+        //
+        //     // const variable = isAudioInput ? currentInputDeviceID : currentOutputDeviceID;
+        //     isAudioInput ? currentInputDeviceID = device.deviceId : currentOutputDeviceID = device.deviceId
+        // }
+
         const targetMap = isAudioInput ? inputDeviceMap : outputDeviceMap;
         targetMap.set(device.label, device.deviceId);
 
         const option = document.createElement("option");
-        option.textContent = device.label;
+        option.value = option.textContent = device.label;
 
         const targetSelect = isAudioInput ? audioInputDeviceSelect : audioOutputDeviceSelect;
         targetSelect.appendChild(option);
     }
+
+    console.log(currentInputDeviceID, currentOutputDeviceID);
 }
+
+
+[audioInputDeviceSelect, audioOutputDeviceSelect].forEach((select) =>
+{
+    const isInput = select === audioInputDeviceSelect;
+
+    select.addEventListener("change", async () =>
+    {
+        const label = select.value;
+        const id = isInput ? inputDeviceMap.get(label) : outputDeviceMap.get(label);
+
+        isInput ? currentInputDeviceID = id : currentOutputDeviceID = id;
+
+        stop();
+        await play();
+
+        console.log(currentInputDeviceID, currentOutputDeviceID);
+    });
+});
 
 
 (async () =>
